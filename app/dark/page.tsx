fe67671dfd595de4e9a35f2099a473a577c75005
Luri <yv.abreu@proton.me>
2026-02-24 13:52:26 -0500

    Establish /light and /dark as dual root homepages

---
+++ app/dark/page.tsx
@@ -0,0 +1,9 @@
+export default function DarkPage() {
+  return (
+    <main>
+      <h1>Darklibra</h1>
+      <p>A Tale of 7 Mirror.</p>
+    </main>
+  );
+}
+
---
+++ app/light/page.tsx
@@ -0,0 +1,9 @@
+export default function LightPage() {
+  return (
+    <main>
+      <h1>Liibra</h1>
+      <p>A Brazilian Legal search engine.</p>
+    </main>
+  );
+}
+
